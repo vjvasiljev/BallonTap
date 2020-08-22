7661ad4dcc70a0697762da8bdf60164e68128f48
Viaceslav <v.j.vasiljev@gmail.com>
2020-08-22 11:14:43 +0300

    add score for each commit tap

--- main.lua
+++ main.lua
@@ -4,9 +4,15 @@
 --
 -----------------------------------------------------------------------------------------
 -- Your code here
+local tapCount = 0;
+
 local background = display.newImageRect("background.png", 360, 570);
 background.x = display.contentCenterX;
 background.y = display.contentCenterY;
+
+local tapText = display.newText(tapCount, display.contentCenterX, 20,
+                                native.systemFont, 40);
+tapText:setFillColor(0, 0, 0);
 
 local platform = display.newImageRect("platform.png", 300, 50);
 platform.x = display.contentCenterX;
@@ -21,10 +27,12 @@ local physics = require('physics');
 physics.start()
 
 physics.addBody(platform, 'static');
-physics.addBody(balloon, "dynamic", {radius = 50, bounce = 0.5});
+physics.addBody(balloon, "dynamic", {radius = 50, bounce = 0.3});
 
 local function pushBalloon()
-    balloon:applyLinearImpulse(0, -1, balloon.x, balloon.y)
+    balloon:applyLinearImpulse(0, -0.75, balloon.x, balloon.y)
+    tapCount = tapCount + 1
+    tapText.text = tapCount
 end
 
 balloon:addEventListener('tap', pushBalloon)
